36997db58f7f30c28273d34d6be28508832bce2c
KozerT <kozer.vizastaff@gmail.com>
2024-12-01 20:23:40 +0100

    feat: install and register a JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.AUTH_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+
   providers: [LocalStrategy],
   controllers: [AuthController],
 })
