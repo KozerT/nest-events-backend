b747a8a4fab4cbeb4a21ca3ee5340f0d2eb3543a
KozerT <kozer.vizastaff@gmail.com>
2024-12-01 19:36:00 +0100

    feat: created a post method with the gueard for the auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  async login(
+    @Request()
+    request,
+  ) {
+    return {
+      userId: request.user.id,
+      token: 'Placeholder for the token',
+    };
+  }
+}
