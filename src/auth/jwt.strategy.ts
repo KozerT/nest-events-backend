406bf1d627e2b08060d5a81258a03b66ba50b540
KozerT <kozer.vizastaff@gmail.com>
2024-12-01 20:46:56 +0100

    feat: create and configured Jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.AUTH_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.userRepository.findOne(payload.sub);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
+  }
+}
